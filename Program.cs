93d4bfb83a0d56c0389185381ed3492b9058d803
AndreyKerges <andrey.nascimento4@etec.sp.gov.br>
2023-04-02 14:06:49 -0300

    exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+
+namespace exe_while_3tpa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int n1 = 0;
+            int n2 = 0;
+
+            Console.Write("informe o 1 número :");
+            n1 = int.Parse(Console.ReadLine()!);
+
+            Console.Write("informe o 2 número :");
+            n2 = int.Parse(Console.ReadLine()!);
+
+            if (n2 >= n1)
+            {
+                int n = n1;
+            while (n <= n2)
+            {
+                Console.WriteLine(n);
+                n += 1;
+            }
+            }
+            else
+            {
+                Console.WriteLine("Erro. O segundo número não pode ser menor que o primeiro.");
+            }
+        }
+    }
+}
